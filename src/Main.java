ba6ca790a1e94c572f1c6b02dc6f1f6d4ef3f1df
AnF <anfwork007@gmail.com>
2023-06-08 00:19:59 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketCost = 15250;
+        final int ONEBONUSMIL = 20;
+        int earnedMils = ticketCost / ONEBONUSMIL;
+        System.out.println("У Вас "+ earnedMils +" бесплатных миль для путешествий!");
+    }
+}
